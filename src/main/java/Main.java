1bad7501830f5ed567015a1e2f2f073a4feb687f
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-11-12 11:01:30 +0300

    Finished project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextLine();
+
+        try {
+            int a1 = Integer.parseInt(a);
+        }
+        catch (Exception E){
+            System.out.println("Not valid");
+        }
+
+        b = scanner.nextLine();
+        try {
+            int b1 = Integer.parseInt(b);
+        }
+        catch (Exception E){
+            System.out.println("Not valid");
+        }
+
+        c = scanner.nextLine();
+
+        try {
+            int c1 = Integer.parseInt(c);
+        }
+        catch (Exception E){
+            System.out.println("Not valid");
+        }
+
+
+
     }
 }
